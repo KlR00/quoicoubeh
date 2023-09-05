73e8ac7ae148878c9934bca012a54255c1151f78
KlR00 <teixeiracarolina181@gmail.com>
2023-09-05 16:27:13 +0200

    commit du 05.09

--- quoicoubeh/Program.cs
+++ quoicoubeh/Program.cs
@@ -11,8 +11,20 @@ namespace calculatrice
         static void Main(string[] args)
         {
 
+            Console.WriteLine("entrer votre premier chiffre");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("entrer votre deuxieme chiffre");
 
-            Console.WriteLine("hello world");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("entrer l'opération (+,-,*,/) :");
+            string opération = Console.ReadLine();
+
+            switch (opération)
+            {
+                case "+":
+
+
+
 
         }
     }
