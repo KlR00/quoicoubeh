3f94c3c5ec41be0e323d6d82b53d60e3b8d91774
KlR00 <teixeiracarolina181@gmail.com>
2023-09-07 10:08:03 +0200

    commit du 07

--- quoicoubeh/Program.cs
+++ quoicoubeh/Program.cs
@@ -11,21 +11,53 @@ namespace calculatrice
         static void Main(string[] args)
         {
 
-            Console.WriteLine("entrer votre premier chiffre");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("entrer votre deuxieme chiffre");
+            
+            
+            
 
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("entrer l'opération (+,-,*,/) :");
-            string opération = Console.ReadLine();
+           int res;
+           Console.WriteLine("entrer votre premier chiffre");
+           int num1 = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("entrer votre deuxieme chiffre");
 
-            switch (opération)
-            {
-                case "+":
+           int num2 = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("entrer l'opération (+,-,*,/) :");
+           string opération = Console.ReadLine();
 
+           switch (opération)
+           { 
 
+              case "+":
 
+                res = num1 + num2;
+                Console.WriteLine("Addition:" + res);
+                break;
 
+              case "-":
+
+                res = num1 - num2;
+                Console.WriteLine("Subtraction:" + res);
+                break;
+
+              case "*":
+
+                res = num1 * num2;
+                Console.WriteLine("addition:" + res);
+                break;
+
+              case "/":
+
+                res = num1 / num2;
+                Console.WriteLine("division:" + res);
+                break;
+
+           }
+             Console.ReadLine();
+               
+            
+            
+
+            
         }
     }
 }
